bd49374d684971df82b441c12a2429b6b8590b01
adnan <adnan4456@gmail.com>
2023-10-14 13:31:11 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+
+import 'dart:ui';
+
+const kPrimaryColor = Color(0xFF0C9869);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
+
---
+++ lib/presentation/home_screen.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatefulWidget{
+
+  const HomeScreen({super.key});
+
+  @override
+   createState() => _HomeScreen();
+}
+class _HomeScreen extends State<HomeScreen>{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: _buildAppBar(),
+
+    );
+  }
+
+  AppBar _buildAppBar(){
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: (){},),
+    );
+  }
+}
